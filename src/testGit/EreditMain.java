91e3a3c1c514dc7fdeddecaca2b2b3a88629f7e7
unknown <giovanni.forlini@studio.unibo.it>
2022-10-03 11:20:42 +0200

    primo commit

---
+++ src/testGit/EreditMain.java
@@ -0,0 +1,10 @@
+package testGit;
+
+public class EreditMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Secondo messaggio");
+	}
+
+}
