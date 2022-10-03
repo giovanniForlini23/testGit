3408ae72881b5a8ab57196d6e9ffbb96272a6257
giovanniForlini23 <giovanni.forlini@studio.unibo.it>
2022-10-03 11:31:49 +0200

    aggiunto terzo messaggio

--- src/testGit/EreditMain.java
+++ src/testGit/EreditMain.java
@@ -5,6 +5,7 @@ public class EreditMain {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Secondo messaggio");
+		System.out.println("Terzo messaggio");
 	}
 
 }
